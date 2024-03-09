06b20d51e4a05f4a556dd03eddba24fcfcc3c8e5
Valentine Wawira <valentinewawira20@gmail.com>
2024-03-09 23:24:12 +0300

    add pages sub-directory main.py and requirement.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import  streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+    page_title="EMBEDDING ML MODELS IN GUIs",
+    page_icon="🌍",
+    layout="wide"
+)
+
+st.title('EMBEDDING ML MODEL IN GUIs')
+st.subheader('Customer Churn Prediciton app')
+st.text_input('username')
+
+
+
+
+
+
+
+
+
+
---
+++ pages/03_predict.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="PREDICT",
+    page_icon="",
+    layout="wide"
+)
+
+st.title('prediction')
